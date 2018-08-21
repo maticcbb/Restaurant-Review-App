837bd97c002e53f7f66e79dc30cc22e25e08a827
Mateusz Mielcarz <m.mielcarz93@gmail.com>
2018-08-21 13:17:26 +0200

    Add Mapbox token

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoibWF0aWNjYmIiLCJhIjoiY2psM2wxcWkzMjJlaTNwcDRxamthanVnMSJ9.ICrhoCAZM8205R2Y5E3_AQ',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
